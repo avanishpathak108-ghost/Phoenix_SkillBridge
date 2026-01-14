e36c6143db77a8960bbfec29dcfd72d14e0b337f
SiLvErPeNgUiN <avanishpathak108@gmail.com>
2026-01-14 15:47:17 +0530

    Create roles_skills.py

---
+++ roles_skills.py
@@ -0,0 +1,26 @@
+ROLES_SKILLS = {
+    "Data Analyst": [
+        "python", "sql", "excel", "statistics",
+        "data visualization", "pandas"
+    ],
+
+    "Web Developer": [
+        "html", "css", "javascript",
+        "react", "backend basics", "git"
+    ],
+
+    "Cybersecurity Analyst": [
+        "networking", "linux", "security fundamentals",
+        "threat analysis", "firewalls", "incident response"
+    ],
+
+    "UI/UX Designer": [
+        "design principles", "wireframing", "prototyping",
+        "figma", "user research", "usability testing"
+    ],
+
+    "Cloud Engineer": [
+        "cloud fundamentals", "aws", "virtualization",
+        "networking", "linux", "devops basics"
+    ]
+}
